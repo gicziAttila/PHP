351f21b29a6040766229b8108c087580091e80ca
giczi_attila <giczi.attila.istvan.2020i@bankitatabanya.hu>
2024-11-28 11:23:25 +0100

    Hibakezelés

--- common/db.inc.php
+++ common/db.inc.php
@@ -9,7 +9,16 @@ $dbname = "php_teszt";
 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+try{
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+}
+catch (Exception $e) {
+    echo "Hiba: " .  $e->getMessage();
+    exit;
+}
 
 // Check connection
 if ($conn->connect_error) {
